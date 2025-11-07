39bd2379a8036dec17ab6d9fd024df0801162ccd
Duansg <duansg@apache.org>
2025-11-07 11:46:24 +0800

    [init]Add configuration directory initialization

---
+++ manager/cli/install.go
@@ -0,0 +1,28 @@
+package cli
+
+import (
+	"path/filepath"
+)
+
+const (
+	DefaultConfigFileName = "config.yaml"
+)
+
+var (
+	ConfigFileDir  = "/conf/"
+	UploadFilePath = "/uploads/"
+	I18nPath       = "/i18n/"
+	CacheDir       = "/cache/"
+)
+
+func FormatAllPath(dataDirPath string) {
+	ConfigFileDir = filepath.Join(dataDirPath, ConfigFileDir)
+	UploadFilePath = filepath.Join(dataDirPath, UploadFilePath)
+	//I18nPath = filepath.Join(dataDirPath, I18nPath)
+	//CacheDir = filepath.Join(dataDirPath, CacheDir)
+}
+
+// GetConfigFilePath get config file path
+func GetConfigFilePath() string {
+	return filepath.Join(ConfigFileDir, DefaultConfigFileName)
+}
